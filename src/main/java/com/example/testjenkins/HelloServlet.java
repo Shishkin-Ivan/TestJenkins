d96d6d6a9251f5e9504c83905003fdbfbd411779
shishkin <craz9.bunn9@yandex.ru>
2025-02-18 22:32:05 +0300

    refactor: change message

--- src/main/java/com/example/testjenkins/HelloServlet.java
+++ src/main/java/com/example/testjenkins/HelloServlet.java
@@ -10,7 +10,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Test Jenkins!";
+        message = "Hello Jenkins!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
